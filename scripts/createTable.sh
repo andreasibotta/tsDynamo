5fed6dbeb3d8ee24908c46ca267ef03dde365949
Andreas Lidholm <andreas.lidholm@ibotta.com>
2020-05-06 07:55:10 -0600

    Script for creating the correct table structure

--- scripts/createTable.sh
+++ scripts/createTable.sh
@@ -1,5 +1,17 @@
+
+aws dynamodb --endpoint-url http://localhost:4569  delete-table --table-name testTable
+
 aws dynamodb --endpoint-url http://localhost:4569  create-table \
     --table-name testTable \
-    --attribute-definitions AttributeName=Artist,AttributeType=S AttributeName=SongTitle,AttributeType=S \
-    --key-schema AttributeName=Artist,KeyType=HASH AttributeName=SongTitle,KeyType=RANGE \
-    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+    --attribute-definitions AttributeName=pk,AttributeType=S AttributeName=sk,AttributeType=S \
+    --key-schema AttributeName=pk,KeyType=HASH AttributeName=sk,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 
+
+aws dynamodb --endpoint-url http://localhost:4569 update-table \
+    --table-name testTable \
+    --attribute-definitions AttributeName=gsi1pk,AttributeType=S  AttributeName=gsi1sk,AttributeType=S \
+    --global-secondary-index-updates \
+    "[{\"Create\":{\"IndexName\": \"gsi1\",\"KeySchema\":[{\"AttributeName\":\"gsi1pk\",\"KeyType\":\"HASH\"}], \
+    \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
+
+       
